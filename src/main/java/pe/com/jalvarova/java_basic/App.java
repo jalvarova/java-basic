49e0e778ad693b4ff63b649e1487867b48780695
ALVARO AGUINAGA DELGADO <alvaro92a18@gmail.com>
2018-09-05 21:22:51 -0500

    feat: docs and books

---
+++ src/main/java/pe/com/jalvarova/java_basic/App.java
@@ -0,0 +1,13 @@
+package pe.com.jalvarova.java_basic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
